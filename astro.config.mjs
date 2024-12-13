6a5b245af38eef9131fe86750b0f7972b010c0ec
Jelenkee <ichhabadhs@posteo.de>
2024-12-13 22:22:45 +0100

    unocss

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 import svelte from '@astrojs/svelte';
-
 import vercel from '@astrojs/vercel';
+import unocss from 'unocss/astro'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
+  integrations: [svelte(), unocss({injectReset: true})],
   adapter: vercel()
 });
--- src/components/create.svelte
+++ src/components/create.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
+    import { actions } from "astro:actions";
+    const INPUT_PREFIX = "ii_";
     let title: string = $state("");
     let candidates: string[] = $state([""]);
-    import { actions } from "astro:actions";
 
     function change() {
         const last = candidates[candidates.length - 1];
@@ -14,6 +15,13 @@
         }
     }
 
+    function enter(event: KeyboardEvent, index: number) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            document.getElementById(INPUT_PREFIX + (index + 1))?.focus();
+        }
+    }
+
     $effect(() => {
         if (candidates == null || candidates.length === 0) {
             candidates = [""];
@@ -22,19 +30,24 @@
 </script>
 
 <div>
-    <div>title</div>
     <input type="text" bind:value={title} />
     <div>
         {#each candidates as _candidate, i}
             <div>
                 {i + 1}:
                 <input
+                    id={INPUT_PREFIX + i}
                     type="text"
                     bind:value={candidates[i]}
                     oninput={change}
+                    onkeypress={(event) => enter(event, i)}
                 />
                 {#if i > 0}
-                    <button onclick={() => candidates.splice(i, 1)}>x</button>
+                    <button
+                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                        onclick={() => candidates.splice(i, 1)}
+                        tabindex="-1">x</button
+                    >
                 {/if}
             </div>
         {/each}
