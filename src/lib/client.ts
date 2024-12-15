fd836bc3a040890e2f01eec20222e8ec0e623cf3
Jelenkee <ichhabadhs@posteo.de>
2024-12-15 15:21:50 +0100

    form

---
+++ src/lib/client.ts
@@ -0,0 +1,24 @@
+import { ActionError, ActionInputError, isActionError } from "astro:actions";
+import { toast } from "@zerodevx/svelte-toast";
+
+export function handleError(error: Error) {
+    let message;
+    if (error instanceof ActionInputError && error.fields) {
+        message = Object.keys(error.fields)
+            .map(key => `${key}: ${error.fields[key]}`)
+            .join("\n")
+    } else if (error instanceof ActionError) {
+        message = error.message;
+    } else {
+        message = "Server Error"
+    }
+    if (message) {
+        toast.push(message, {
+            theme: {
+                "--toastColor": "white",
+                "--toastBackground": "red",
+                "--toastBarBackground": "darkred",
+            },
+        });
+    }
+}
--- uno.config.ts
+++ uno.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig, presetUno } from "unocss";
+import presetIcons from "@unocss/preset-icons";
 
 export default defineConfig({
     presets: [
-        presetUno()
+        presetUno(), presetIcons()
     ],
     shortcuts: {
-        "input":"bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5",
-        "button":"text-white bg-blue-700 enabled:hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 disabled:cursor-not-allowed disabled:opacity-50"
+        "input": "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ",
+        "button": "text-white bg-blue-700 enabled:hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-1 me-2 disabled:cursor-not-allowed disabled:opacity-50"
     }
 })
